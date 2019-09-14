09ec6096619318daf62250905aba235da9979d3d
Adrian <adekenn3@gmail.com>
2019-09-14 17:42:31 +0200

    getting sprites from script

---
+++ Game/Assets/Scripts/GameManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class GameManager : MonoBehaviour
+{
+    [Header("Sprites")]
+    public Sprite[] binSprites;
+    public Sprite[] trashSprites;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public Sprite getBinSprite(int index)
+    {
+        return binSprites[index];
+    }
+
+    public Sprite getTrashSprite(int index)
+    {
+        return trashSprites[index];
+    }
+}
