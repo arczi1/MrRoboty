3b28d92b2ef03df4d0799f9865a484d5b64c7844
Adrian <adekenn3@gmail.com>
2019-09-14 17:04:26 +0200

    added trash and bin

---
+++ Game/Assets/Scripts/BinScript.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BinScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void setTag(Trash.TrashType type)
+    {
+        switch(type)
+        {
+            case Trash.TrashType.PLASTIC:
+                tag = "PlasticBin";
+                break;
+            case Trash.TrashType.PAPER:
+                tag = "PaperBin";
+                break;
+            case Trash.TrashType.GLASS:
+                tag = "GlassBin";
+                break;
+            case Trash.TrashType.ORGANIC:
+                tag = "OrganicBin";
+                break;
+            case Trash.TrashType.NON_RECYCLABLE:
+                tag = "Non-RecyclableBin";
+                break;
+            case Trash.TrashType.METAL:
+                tag = "MetalBin";
+                break;
+            case Trash.TrashType.BATTERY:
+                tag = "BatteryBin";
+                break;
+            case Trash.TrashType.CUP:
+                tag = "CupBin";
+                break;
+            default:
+                tag = "NullBin";
+                break;
+        }
+    }
+}
---
+++ Game/Assets/Scripts/Trash.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trash : MonoBehaviour
+{
+    private TrashType type;
+
+    public enum TrashType
+    {
+        PLASTIC,
+        PAPER,
+        GLASS,
+        ORGANIC,
+        NON_RECYCLABLE,
+        METAL,
+        AGD,
+        BATTERY,
+        CUP,
+        NULL
+    }
+    public Trash(TrashType type)
+    {
+        this.type = type;
+    }
+}
